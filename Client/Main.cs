f2ac9f9d4eb90105ebe6d041b79497eeb623df55
Brynner <bventura03@hotmail.com>
2023-12-09 03:09:52 -0500

    Fixed an issue where users were allowed to create an infinite amount of bodyguards. Also added command to remove a bodyguard if it there is one active.

--- Client/Main.cs
+++ Client/Main.cs
@@ -6,26 +6,56 @@ namespace Client
 {
     public class Main : BaseScript
     {
+        private int bodyguardHandle = 0;
+        private bool isBodyguardActive = false;
+
         public Main()
         {
-            API.RegisterCommand("bodyguard", new Action(TestCommand), false);
+            API.RegisterCommand("bg_create", new Action(CreateBodyguard), false);
+            API.RegisterCommand("bg_remove", new Action(RemoveBodyguard), false);
         }
 
-        private async static void TestCommand()
+        private async void CreateBodyguard()
         {
-            Ped player = Game.Player.Character;
-            API.RequestModel((uint)PedHash.ChemSec01SMM);
-            while (!API.HasModelLoaded((uint)PedHash.ChemSec01SMM))
+            if (!isBodyguardActive)
             {
-                Debug.WriteLine("Waiting for model to load");
-                await BaseScript.Delay(100);
+                Ped player = Game.Player.Character;
+                API.RequestModel((uint)PedHash.ChemSec01SMM);
+                while (!API.HasModelLoaded((uint)PedHash.ChemSec01SMM))
+                {
+                    Debug.WriteLine("Waiting for model to load");
+                    await BaseScript.Delay(100);
+                }
+                Ped bodyguard = await World.CreatePed(PedHash.ChemSec01SMM, player.Position + (player.ForwardVector * 2));
+                if (bodyguard != null && bodyguard.Exists())
+                {
+                    bodyguard.Task.LookAt(player);
+                    API.SetPedAsGroupMember(bodyguard.Handle, API.GetPedGroupIndex(player.Handle));
+                    API.SetPedCombatAbility(bodyguard.Handle, 2);
+                    API.GiveWeaponToPed(bodyguard.Handle, (uint)WeaponHash.AssaultRifleMk2, 500, false, true);
+                    bodyguard.PlayAmbientSpeech("GENERIC_HI");
+                    bodyguardHandle = bodyguard.Handle;
+                    isBodyguardActive = true;
+                }
+            } else
+            {
+                API.SendNuiMessage("A bodyguard is already active!");
             }
-            Ped bodyguard = await World.CreatePed(PedHash.ChemSec01SMM, player.Position + (player.ForwardVector * 2));
-            bodyguard.Task.LookAt(player);
-            API.SetPedAsGroupMember(bodyguard.Handle, API.GetPedGroupIndex(player.Handle));
-            API.SetPedCombatAbility(bodyguard.Handle, 2);
-            API.GiveWeaponToPed(bodyguard.Handle, (uint)WeaponHash.AssaultRifleMk2, 500, false, true);
-            bodyguard.PlayAmbientSpeech("GENERIC_HI");
+        }
+
+        private async void RemoveBodyguard()
+        {
+            if (isBodyguardActive)
+            {
+                await BaseScript.Delay(0);
+                API.DeletePed(ref bodyguardHandle);
+                bodyguardHandle = 0;
+                isBodyguardActive = false;
+                Debug.WriteLine("The bodyguard has been removed!");
+            } else
+            {
+                Debug.WriteLine("There is no active bodyguard to remove.");
+            }
         }
     }
 }
